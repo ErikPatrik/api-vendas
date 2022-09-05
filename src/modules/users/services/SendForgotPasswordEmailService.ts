94c72b5f9ac804c863a9ff5f79260591b2b67c19
erik <erik@meucrediario.com.br>
2022-09-05 13:25:45 -0300

    Install etherealmail and handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from "typeorm"
 import AppError from "../../../shared/errors/AppError"
 import { UsersRepository } from "../typeorm/repositories/UsersRepository"
 import { UserTokensRepository } from "../typeorm/repositories/UserTokensRepository"
+import EtherealMail from '../../../config/mail/EtherealMail'
 
 interface Irequest {
     email: string
@@ -21,6 +22,11 @@ class SendForgotPasswordEmailService {
         const token = await userTokensRepository.generate(user.id)
 
         console.log(token)
+
+        await EtherealMail.sendMail({
+            to: email,
+            body: `Received password reset request: ${token?.token}`
+        })
     }
 }
 
