cd8f778d7b0d2ff2bff922ec3e157e0257bf18a6
erik <erik@meucrediario.com.br>
2022-09-06 15:07:05 -0300

    finalizado envio de email para recuperação de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from "typeorm"
 import AppError from "../../../shared/errors/AppError"
+import path from "path"
 import { UsersRepository } from "../typeorm/repositories/UsersRepository"
 import { UserTokensRepository } from "../typeorm/repositories/UserTokensRepository"
 import EtherealMail from '../../../config/mail/EtherealMail'
@@ -19,13 +20,23 @@ class SendForgotPasswordEmailService {
             throw new AppError('User does not exists')
         }
 
-        const token = await userTokensRepository.generate(user.id)
+        const { token } = await userTokensRepository.generate(user.id)
 
-        console.log(token)
+        const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs')
 
         await EtherealMail.sendMail({
-            to: email,
-            body: `Received password reset request: ${token?.token}`
+            to: {
+                name: user.name,
+                email: user.email,
+            },
+            subject: '[API Vendas] Password recovery',
+            templateData: {
+                file: forgotPasswordTemplate,
+                variables: {
+                    name: user.name,
+                    link: `http://localhost:3000/reset_password?token=${token}`,
+                }
+            }
         })
     }
 }
